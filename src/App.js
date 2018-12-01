e6db9475bf8bae1b8d8d69f757a25646e1c93a69
naman0105 <naman0105@gmail.com>
2018-12-01 12:16:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StudentApplyForm from './components/student_apply.js';
+import CompanyRecruitmentForm from './components/company_recruitment_form.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        	<StudentApplyForm></StudentApplyForm>
+        	<CompanyRecruitmentForm></CompanyRecruitmentForm>
+        </header>        
       </div>
     );
   }
