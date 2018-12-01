714e15b138314230674d6d156352bc9c77c1494e
Bhavya Jain <bhavya16081997@gmail.com>
2018-12-01 14:54:41 +0530

    commit

--- src/components/company_recruitment_form.js
+++ src/components/company_recruitment_form.js
@@ -5,48 +5,56 @@ class CompanyRecruitmentForm extends Component {
   render() {
     return (
       <Form>
+        <FormGroup>
+          <Label for="com_name">Company Name</Label>
+            <Input type="text" name="com_name" id="com_name" placeholder="Enter your compnay's name" />
+        </FormGroup>
         <Row form>
           <Col md={6}>
             <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+              <Label for="website">Website</Label>
+              <Input type="text" name="website" id="website" placeholder="Enter your company's website" />
             </FormGroup>
           </Col>
           <Col md={6}>
             <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+              <Label for="year">Year</Label>
+              <Input type="text" name="year" id="year" />
             </FormGroup>
           </Col>
         </Row>
         <FormGroup>
-          <Label for="exampleAddress">Address</Label>
-          <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleAddress2">Address 2</Label>
-          <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+          <Label for="com_desc">Description</Label>
+          <Input type="text" name="com_desc" id="com_desc"/>
         </FormGroup>
         <Row form>
-          <Col md={6}>
+          <Col md={4}>
             <FormGroup>
-              <Label for="exampleCity">City</Label>
-              <Input type="text" name="city" id="exampleCity"/>
+              <Label for="pos_avl">Position offered</Label>
+              <Input type="text" name="pos_avl" id="pos_avl"/>
             </FormGroup>
           </Col>
           <Col md={4}>
             <FormGroup>
-              <Label for="exampleState">State</Label>
-              <Input type="text" name="state" id="exampleState"/>
+              <Label for="pos_time">Starting date</Label>
+              <Input type="date" name="pos_time" id="pos_time"/>
             </FormGroup>
           </Col>
-          <Col md={2}>
+          <Col md={4}>
             <FormGroup>
-              <Label for="exampleZip">Zip</Label>
-              <Input type="text" name="zip" id="exampleZip"/>
-            </FormGroup>  
+              <Label for="pos_place">Place</Label>
+              <Input type="text" name="pos_place" id="pos_place"/>
+            </FormGroup>
           </Col>
         </Row>
+        <FormGroup>
+          <Label for="pos_desc">City</Label>
+          <Input type="text" name="pos_desc" id="pos_desc"/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="skill">Skills required</Label>
+          <Input type="text" name="skill" id="skill"/>
+        </FormGroup>
         <FormGroup check>
           <Input type="checkbox" name="check" id="exampleCheck"/>
           <Label for="exampleCheck" check>Check me out</Label>
